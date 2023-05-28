38e0cd4d5c8e62f5ec3b72ca5bedcebe405936b0
Pablo Horta <rigohorta@gmail.com>
2023-05-28 16:23:54 -0300

    CREATE models en home

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class cita_agenda(models.Model):
+    fecha = models.DateField()
+    motivo_consulta = models.TextField()
+    
+    def __str__(self):
+        return self.fecha
+    
+    
+class autor(models.Model):
+    nombre = models.CharField(max_length=50)
+    motivo_consulta = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+    
+    
+class cliente(models.Model):
+    nombre_mascota = models.CharField(max_length=50)
+    raza = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre_mascota
