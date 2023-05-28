92874a6bbcf01f9725ab7b807531371590838fa7
Pablo Horta <rigohorta@gmail.com>
2023-05-28 16:42:12 -0300

    registro de modelos

--- project/apps/home/admin.py
+++ project/apps/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.cita_agenda)
+admin.site.register(models.autor)
+admin.site.register(models.cliente)
